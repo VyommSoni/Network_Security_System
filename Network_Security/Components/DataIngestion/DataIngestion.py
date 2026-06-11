4efebff933637b93d6913000e1a278968c6a1d84
vyomsoni <svyom21@gmail.com>
2026-06-11 19:24:11 +0530

    Data Ingestion

--- Network_Security/Components/DataIngestion/DataIngestion.py
+++ Network_Security/Components/DataIngestion/DataIngestion.py
@@ -27,17 +27,17 @@ class DataIngestion:
         try:
             train,test=train_test_split(data,test_size=self.data_ingestion_config.split_ratio,random_state=42)
 
-            os.makedirs(os.path.dirname(self.data_ingestion_config.train_file_path),exist_ok=True)
-
             train.to_csv(
-                self.data_ingestion_config.train_file_path,header=True,index=False
+                self.data_ingestion_config.train_file_path,index=False
             )
+            print(f"train data {train}")
+            print(f"test data {test}")
 
             test.to_csv(
-                self.data_ingestion_config.test_file_path,header=True,index=False
+                self.data_ingestion_config.test_file_path,index=False
             )
-            return train,test
-        
+            logging.info(f"successfully wrote the train and test data into the location {self.data_ingestion_config.train_file_path} and {self.data_ingestion_config.test_file_path}")
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
@@ -77,19 +77,22 @@ class DataIngestion:
             if not data.empty:
                 logging.info("Exported data from mongodb in as a dataframe")
 
-                os.makedirs(os.path.dirname(self.data_ingestion_config.feature_store_filepath),exist_ok=True)#making folder
+                os.makedirs(os.path.dirname(self.data_ingestion_config.data_ingestion_dir),exist_ok=True)#making folder
+
+                os.makedirs(self.data_ingestion_config.ingested_data_dir,exist_ok=True)
                 data.to_csv(self.data_ingestion_config.feature_store_filepath,index=False,header=True)
                 
                 #split dataframe
-                train,test=self.Split_into_train_and_test(data=data)
+                self.Split_into_train_and_test(data=data)
 
-                data_ingestion_artifact=DataIngestionArtifact(train_filepath=self.data_ingestion_config.train_file_path,
-                                                              test_filepath=self.data_ingestion_config.test_file_path,
+                data_ingestion_artifact=DataIngestionArtifact(train_file_path=self.data_ingestion_config.train_file_path,
+                                                              test_file_path=self.data_ingestion_config.test_file_path,
                                                               feature_storepath=self.data_ingestion_config.feature_store_filepath
                 )
 
                 
-
+                logging.info(f"artifact {data_ingestion_artifact}")
+                print(f"artifact {data_ingestion_artifact}")
                 return data_ingestion_artifact
 
             logging.info("Data exported from mongodb is empty,Please troubleshoot that problem...")
@@ -99,6 +102,3 @@ class DataIngestion:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-if __name__== "__main__":
-    DT=DataIngestion(data_ingestion_config=DataIngestionConfig(training_pipeline_config=TrainingPipelineConfig()))
-    DT.Initiate_Data_Ingestion()
